87a9e672edd096d0e18a2d67d6f4d4ff0527c953
Saurav Telge <18sauravt@gmail.com>
2021-04-29 13:55:38 +0530

    Flask website

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template, Response
-import cv2
-camera = cv2.VideoCapture(0)
+from flask import Flask, render_template, request, redirect, jsonify,Response
 from keras.models import load_model
 from collections import deque
 import numpy as np
@@ -10,7 +8,7 @@ import cv2
 import os
 from random import randrange
 from sklearn.preprocessing import LabelBinarizer
-from google.colab.patches import cv2_imshow
+# from google.colab.patches import cv2_imshow
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,12 +17,15 @@ from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from PIL import Image as im
-
+UPLOAD_FOLDER = "E:/LBS/website/lbs/static/example_clips"
 app = Flask(__name__)
-img_dir = '/assets/output_images/'
-model = '/assets/model/activity.model'
-label_bin = '/assets/model/lb.pickle'
-input_dir = '/assets/example_clips/cctv.mp4'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+global path
+path = '' 
+img_dir = 'E:/LBS/website/lbs/static/output_images/'
+model = 'E:/LBS/website/lbs/assets/model/activity.model'
+label_bin = 'E:/LBS/website/lbs/assets/model/lb.pickle'
+input_dir = 'E:/LBS/website/lbs/static/example_clips/testeer.mp4'
 size = 128
 model = load_model(model)
 lb = pickle.loads(open(label_bin, "rb").read())
@@ -35,20 +36,23 @@ def gen_frames():
 
     # initialize the video stream, pointer to output video file, and
     # frame dimensions
-    vs = cv2.VideoCapture(input_dir)
+    global path
+    vs = cv2.VideoCapture(path)
     writer = None
     (W, H) = (None, None)
     counter = 0
     def getFrame(sec):
-        vs.set(cv2.CAP_PROP_POS_MSEC,sec*500)
+        vs.set(cv2.CAP_PROP_POS_MSEC,sec*100)
         (grabbed, frame) = vs.read()
         return (grabbed,frame)
 # loop over frames from the video file stream
     secs = 0
+    print("hi0")
     while True:
         # read the next frame from the file
         secs = secs + 1
         (grabbed,frame) = getFrame(secs)
+        print("hi1")
         # if the frame was not grabbed, then we have reached the end
         # of the stream
         if not grabbed:
@@ -64,11 +68,11 @@ def gen_frames():
         output = frame.copy()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.resize(frame, (224, 224)).astype("float32")
-
+        output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
         # make predictions on the frame and then update the predictions
         # queue
         x = image.img_to_array(frame)
-        plt.imshow(x/255.)
+        # plt.imshow(x/255.)
         x = np.expand_dims(x, axis=0)
         preds = model.predict(x)
         print(preds)
@@ -80,16 +84,16 @@ def gen_frames():
         print("i")
         print(i)
         # draw the activity on the output frame
-        text = " "
-        cv2.putText(output, text, (35, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255, 0, 0), 5)
-        cv2_imshow(output)
+      
+        
 
         # capture all the frame of accident and save it in output_images folder
         if (i == 0):
             label = "accident"
             counter = counter + 1
+            print("hi3")
             alert = "warning:{}".format(label)
-            cv2.putText(output, alert, (35, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (0, 255, 0), 5)
+            cv2.putText(output, alert, (35, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255,255,255), 5)
             irand = randrange(0, 1000)
             # write the output image to disk
             filename = "{}.png".format(irand)
@@ -97,10 +101,10 @@ def gen_frames():
             cv2.imwrite(p, output)
 
         if (i == 2):
-            label = "fire"
+            label = "accident"
             counter = counter + 1
             alert = "warning:{}".format(label)
-            cv2.putText(output, alert, (35, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (0, 255, 0), 5)
+            cv2.putText(output, alert, (35, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255,255,255), 5)
             irand = randrange(0, 1000)
             # write the output image to disk
             filename = "{}.png".format(irand)
@@ -108,12 +112,12 @@ def gen_frames():
             """ cv2.imwrite(p, output) """
         
         print("printing output before mailing it")
-        cv2_imshow(output)
+        # cv2.imshow("output",output)
 
         # sending the mail
         if(counter>5):
             print("printing output before mailing it")
-            cv2_imshow(output)
+            # cv2.imshow("output",output)
             strFrom = 'lbtestingacc@gmail.com'
             strTo = '2018.abhijit.thikekar@ves.ac.in'
             msgRoot = MIMEMultipart('related')
@@ -139,12 +143,27 @@ def gen_frames():
             smtp.sendmail(strFrom, strTo, msgRoot.as_string())
             smtp.quit()
             break
-        
+        ret, buffer = cv2.imencode('.jpg', output)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     print("[INFO] cleaning up...")
+ 
+    path = ""
     vs.release()
+@app.route("/upload",methods=["GET", "POST"])
+def upload():
+    f = request.files["file"]
+    f.filename=f.filename.replace(" ","_")
+    f.save(os.path.join(app.config["UPLOAD_FOLDER"], f.filename))
+    global path
+    path = f'E:/LBS/website/lbs/static/example_clips/{f.filename}'
+    return home()
 
 @app.route("/")
 def home():
+
+    
     return render_template("index.html")
 
 @app.route('/video_feed')
